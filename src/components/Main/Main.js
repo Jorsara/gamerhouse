ee84da54e8ce950bea14e6a37756ec09b52d0322
marianobenamo <mariano.benamo@gmail.com>
2021-05-30 20:29:07 -0300

    2da entrega

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../NavBar/NavBar';
+
+function Main() {
+  return (
+    <NavBar />
+  );
+}
+
+export default Main;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+import './NavBar.css';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import logo from '../../assets/img/logo.png';
+import lupa from '../../assets/icons/lupa.png';
+import carrito from '../../assets/icons/carrito.png';
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home"><img src={logo} alt="logo"></img></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#home">Inicio</Nav.Link>
+                <Nav.Link href="#link">Formas de pago</Nav.Link>
+                <NavDropdown title="Tienda" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Juegos para PC</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Juegos PS4 Digital</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Juegos PS5 Digital</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Sale</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#link">FAQs</Nav.Link>
+                <Nav.Link href="#link">Contacto</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Buscar..." className="mr-sm-2" />
+                    <Button variant="outline-success"><img src={lupa} alt="buscar"></img></Button>
+                </Form>
+                <Nav.Link href="#link"><img src={carrito} alt="carrito"></img></Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default NavBar;
