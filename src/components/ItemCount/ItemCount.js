dae35ed09d495905a195f3d81232b7af7703ad2f
marianobenamo <mariano.benamo@gmail.com>
2021-06-10 18:25:32 -0300

    no message

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,42 +1,38 @@
+import {useState} from 'react';
 import './ItemCount.css';
-import {InputGroup, FormControl, Button} from 'react-bootstrap';
+import {InputGroup, Button} from 'react-bootstrap';
 
-function ItemListContainer (props) {
-    document.addEventListener("DOMContentLoaded", function() {
-        let contador = document.getElementById('cantidad');
-        contador.value = props.initial;
-        document.getElementById('restar').addEventListener('click', (e) =>{
-            e.stopImmediatePropagation();
-            if(contador.value > 0){
-                contador.value = Number(contador.value) -1;
-            }
-        });
-        document.getElementById('sumar').addEventListener('click', (e) =>{
-            e.stopImmediatePropagation();
-            if(contador.value < props.stock){
-                contador.value = Number(contador.value) +1;
-            }            
-        });
-        document.getElementById('agregar').addEventListener('click', (e) =>{
-            if(props.stock !== 0 && contador.value <= props.stock){
-                props.onAdd(contador.value);
-            }
-        });
-    });    
+function ItemCount ({initial, stock, onAdd}) {
+    const [contador, setContador] = useState(initial);
+    function restar(){
+        if(contador > 1){
+            setContador(contador - 1);
+        }
+    };
+    function sumar(){
+        if(contador < stock){
+            setContador(contador + 1);
+        }            
+    };
+    function agregar(){
+        if(stock !== 0 && contador <= stock){
+            onAdd(contador);
+        }
+    };
     return (
         <>
             <InputGroup className="mt-4 text-center">
                 <InputGroup.Prepend>
-                    <Button variant="outline-secondary" id="restar">-</Button>
+                    <Button variant="outline-secondary" onClick={()=>restar()}>-</Button>
                 </InputGroup.Prepend>
-                <FormControl id="cantidad" />
+                <input className="form-control" value={contador} readOnly id="cantidad" />
                 <InputGroup.Append>
-                    <Button variant="outline-secondary" id="sumar">+</Button>
+                    <Button variant="outline-secondary" onClick={()=>sumar()}>+</Button>
                 </InputGroup.Append>
             </InputGroup>  
-            <Button variant="outline-secondary" className="mt-2" id="agregar">Agregar al carrito</Button>
+            <Button variant="outline-secondary" className="mt-2" onClick={()=>agregar()}>Agregar al carrito</Button>
         </>
     );
 }
   
-export default ItemListContainer;
+export default ItemCount;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -11,7 +11,7 @@ function ItemListContainer (props) {
             <Row>
                 <Col className="text-center">
                     <h1>{props.greeting}</h1>
-                    <ItemCount stock="5" initial="1" onAdd={() => onAdd()}/>
+                    <ItemCount stock="5" initial={1} onAdd={() => onAdd()}/>
                 </Col>
             </Row>
         </Container>        
